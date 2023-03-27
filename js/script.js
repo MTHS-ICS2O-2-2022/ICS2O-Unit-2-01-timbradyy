e3644f6e12eea1b90827957da4d890650b8b62c7
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-03-27 16:52:52 +0000

    ilovejesussomuch

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tim Brady
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "Hello, World!"
+}
